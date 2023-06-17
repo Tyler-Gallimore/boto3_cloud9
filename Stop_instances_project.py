9cfd2f1716c56c1b29115e7c5c0db48391fd120e
Tyler-Gallimore <lifforn@mailfence.com>
2023-06-17 20:14:25 +0000

    Edited code to stop Dev tags only

--- Stop_instances_project.py
+++ Stop_instances_project.py
@@ -8,22 +8,19 @@ def stop_instance(client, instance_id):
     )
     print(instance_id, "Stopped")
     
-def describe_instance():
+def stop_dev_instance():
 
     response = ec2.describe_instances()
     
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
-            print(instance["InstanceId"], instance["State"]["Name"])
-            
             if "Tags" in instance:
                 for tag in instance["Tags"]:
                     if tag["Key"] == "Environment":
-                        stop_instance(ec2, instance)
+                        if tag["Value"] == "Dev":
+                            stop_instance(ec2, instance["InstanceId"])
 
     
 if __name__ == '__main__':
     ec2 = boto3.client('ec2')
-    #instance_id = "i-0aebcc42034c6dc79"
-    #terminate_instance(ec2, instance_id)
-    describe_instance()
+    stop_dev_instance()
